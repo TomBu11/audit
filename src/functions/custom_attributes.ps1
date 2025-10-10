12a64942c42a033efd118b166b76c79ced497c2d
Tom <tom@rocksalt.cc>
2025-10-10 11:36:20 +0100

    Moved data source and custom attributes itno the main template

--- src/functions/custom_attributes.ps1
+++
@@ -1,3 +0,0 @@
-foreach ($property in $outTable.PSObject.Properties) {
-  Action1-Set-CustomAttribute "$($property.Name)" "$($property.Value)"
-}
--- src/functions/data_source.ps1
+++
@@ -1,5 +0,0 @@
-$result = New-Object System.Collections.ArrayList;
-
-$result.Add($outTable) | Out-Null
-
-$result;
